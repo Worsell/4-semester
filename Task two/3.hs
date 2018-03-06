32247ba08e7be7d76bb85af2ff876acfd5e57c5e
Ivan Korotkii <vana0pub@gmail.com>
2018-03-06 18:51:12 +0300

    Added 3

--- Task two/3.hs
+++ Task two/3.hs
@@ -1,5 +1,5 @@
-f n  = mod n 10
-g n = div n 10
+mod' n  = mod n 10
+div' n = div n 10
 
-function n  |  length (show n) == 1 = n
-			|  otherwise = f n + (function (g n)) 
+summNumber n  |  length (show n) == 1 = n
+			|  otherwise = div' n + (summNumber (mod' n)) 
