baf683cb2feef645e14eccf18846b0b52ff8a739
Ivan Korotkii <vana0pub@gmail.com>
2018-03-06 20:59:24 +0300

    Added five

--- Task two/5.hs
+++ Task two/5.hs
@@ -1,17 +1,17 @@
-added l1
+tail' l1
 	| length l1 == 0 = []
 	| otherwise = tail l1
 
-added' l
+head' l
 	| length l == 0 = 0
 	| otherwise = head l
 
 function' (l1, l2, l3, r) 
 	| (l1 == [] && l2 == [] && l3 == []) = ([],[],[], r)
-	| otherwise  = function' (added l1, added l2, added l3, ((added' l1) + (added' l2) + (added' l3)) : r)
+	| otherwise  = function' (tail' l1, tail' l2, tail' l3, ((head' l1) + (head' l2) + (head' l3)) : r)
 
 
-e (l1,l2,l3,r) = r
+elementary (list1, list2, list3, r) = r
 
 
-function l11 l22 l33 = reverse (e (function' (l11, l22, l33, [])))
+summOfList list11 list22 list33 = reverse (elementary (function' (list11, list22, list33 , []) ))
