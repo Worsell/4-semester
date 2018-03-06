5e42940c08137dd109a440b688f39ddf642d4f7b
Ivan Korotkii <vana0pub@gmail.com>
2018-03-06 19:00:41 +0300

    Added three

--- Task two/3.hs
+++ Task two/3.hs
@@ -1,6 +1,4 @@
-mod' n  = mod n 10
-div' n = div n 10
 
 
 summNumber n  |  length (show n) == 1 = n
-			|  otherwise = div' n + (summNumber (mod' n)) 
+			|  otherwise = (div n 10) + (summNumber (mod n 10)) 
