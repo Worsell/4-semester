b3352a717b11d1415f491c5ed2a4f68724aef6f0
Ivan Korotkii <vana0pub@gmail.com>
2018-03-06 18:52:50 +0300

    Revert "Added 3"
    This reverts commit 32247ba08e7be7d76bb85af2ff876acfd5e57c5e.

--- Task two/3.hs
+++ Task two/3.hs
@@ -1,5 +1,5 @@
-mod' n  = mod n 10
-div' n = div n 10
+f n  = mod n 10
+g n = div n 10
 
-summNumber n  |  length (show n) == 1 = n
-			|  otherwise = div' n + (summNumber (mod' n)) 
+function n  |  length (show n) == 1 = n
+			|  otherwise = f n + (function (g n)) 
