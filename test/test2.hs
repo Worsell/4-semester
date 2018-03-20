b19ab8c7a449e630a39e00bd8ec8477bd15383cb
Ivan Korotkii <vana0pub@gmail.com>
2018-03-20 10:13:14 +0300

    remove 2

--- test/test2.hs
+++
@@ -1,7 +0,0 @@
-matrix :: Integer -> [[Integer]]
-matrix n = map (\x -> matrix' 1 x n) [1..n]
-
-matrix' a x n
-	| a <= x = x : matrix' (a+1) x n
-	| a == (n+1) = []
-	| otherwise = a : matrix' (a + 1) x n
