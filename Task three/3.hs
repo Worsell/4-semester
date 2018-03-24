e9b52b0f544c05f8e17a897306206fe612056ccf
Ivan Korotkii <vana0pub@gmail.com>
2018-03-25 02:33:25 +0300

    Modified 3

--- Task three/3.hs
+++ Task three/3.hs
@@ -1,7 +1,8 @@
+first (x,_,_) = x
 
 findFirstPositionMaxSumm :: [Int] -> Int
-findFirstPositionMaxSumm list = head $ foldl find [0, head list, 0] list where 
-									find [position, summ, base] next = if newSumm > summ
-																						then [position+1, newSumm, next]
-																						else [position, summ, next] where
-																						newSumm = base + next
+findFirstPositionMaxSumm list = first $ foldl find (0, head list, 0) list where 
+		find (position, summ, base) next = if newSumm > summ
+					then (position+1, newSumm, next)
+					else (position, summ, next) where
+					newSumm = base + next
