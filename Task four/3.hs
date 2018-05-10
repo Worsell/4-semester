3027c7822139339d88167716436436877c4015fd
Ivan Korotkii <vana0pub@gmail.com>
2018-05-10 21:02:51 +0300

    modifed 3.hs

--- Task four/3.hs
+++ Task four/3.hs
@@ -1,8 +1,5 @@
--- :set -XderiveFoldable не работает при загрузке из файла, потому пишу тот код, который надо вбить в консоль.
 
-Prelude> :set -XDeriveFoldable
-Prelude> data Foo a = Leaf a | Branch [Foo a] deriving(Show, Foldable)
-Prelude> order = foldr (:) []
--- Пример вызова
-Prelude> order $ Branch[Leaf 2, Leaf 3, Branch[Leaf 4, Leaf 5, Leaf 6]
-[2,3,4,5,6]
+
+{-# LANGUAGE DeriveFoldable #-}
+data Foo a = Leaf a | Branch [Foo a] deriving(Show, Foldable)
+order tree = foldr (:) [] tree
