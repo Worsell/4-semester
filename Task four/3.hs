52b9bc799c556a912c4bf85bd4c38cd7ff78f232
Ivan Korotkii <vana0pub@gmail.com>
2018-05-21 12:43:55 +0300

    add own tree instance

--- Task four/3.hs
+++ Task four/3.hs
@@ -1,5 +1,10 @@
+import Data.Foldable
 
+data Tree a = Empty | Leaf a | Node (Tree a) a (Tree a)
 
-{-# LANGUAGE DeriveFoldable #-}
-data Foo a = Leaf a | Branch [Foo a] deriving(Show, Foldable)
+instance Foldable Tree where
+   foldr f z Empty = z
+   foldr f z (Leaf x) = f x z
+   foldr f z (Node l k r) = foldr f (f k (foldr f z r)) l
+   
 order tree = foldr (:) [] tree
