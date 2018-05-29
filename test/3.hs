e2ff1e3882c6b6d735277be3dc4f5dea249c0ad9
Ivan Korotkii <vana0pub@gmail.com>
2018-05-29 10:13:41 +0300

    Add

---
+++ test/3.hs
@@ -0,0 +1,9 @@
+import System.Random
+import System.IO.Unsafe  -- be careful!                                         
+
+
+randomList :: Int -> [Int]
+randomList seed = randoms (mkStdGen seed) :: [Int]
+
+
+task list = randomIO >>= (\x -> (print $ take (length list) (randomList x)))
